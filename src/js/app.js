4d16c12addfc8abdd3ebe3896492d86066d4a614
axel9802 <u20161a005@upc.edu.pe>
2021-01-25 15:13:19 -0500

    Creando HTML a servicios y Sass a los servicios

--- src/js/app.js
+++ src/js/app.js
@@ -18,13 +18,60 @@ async function mostrarServicios(){
 
         //Generar HTML
         servicios.forEach( servicio => {
-            console.log(servicio);
             /* Aplicando Destructuring:
             Esto es igual a : servicios.id, servicios.nombre, servicios.precio*/
             const{ id, nombre, precio } = servicio;
 
+
+            //DOM Scripting
+            //Generar nombre de servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+
+            //Generar el precio de servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            //Generar div contenedor de Servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+            //Para coger el id
+            servicioDiv.dataset.idServicio = id;
+
+            //Selecciona un servicio para la cita
+            servicioDiv.onclick = seleccionarServicio;
+
+            //Inyectar nombre y precio al div de servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            //Inyectarlo en el HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
+
+            
         })
     } catch (error) {
         console.log(error);
     }
+}
+
+function seleccionarServicio(e){
+
+    let elemento;
+    //Forzar que el elemento al cual le damos click sea el div
+    if(e.target.tagName === 'P'){
+        elemento = e.target.parentElement;
+    } else {
+        elemento = e.target;
+    }
+
+    if(elemento.classList.contains('seleccionado')){
+        elemento.classList.remove('seleccionado');
+    } else {
+        elemento.classList.add('seleccionado');
+    }
+    console.log(elemento)
 }
